c8a3317f38be4b68448e75ac938d4b06295eb226
Victor Manuel <victor@dreamcode.io>
2020-09-08 10:50:05 -0500

    Add dependency to core module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost:27017/nixi"),
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
